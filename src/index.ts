172b9a81c972dea87905992ebee1e69962e0ad92
Gustavo-mts <gm357191@gmail.com>
2025-04-07 22:25:56 -0300

    feat: subindo configurações básicas da api

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import path from 'node:path';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { routes } from './router';
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3006;
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(express.json());
+    app.use(routes)
+
+    app.listen(port, () => {
+      console.log(`servidor rodando em http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('erro ao conectar ao banco de dados'));
---
+++ src/router.ts
@@ -0,0 +1,58 @@
+import { Router } from "express";
+import multer from "multer";
+import path from 'node:path';
+
+import { listCategories } from "./app/useCases/categories/listCategories";
+import { createCategories } from "./app/useCases/categories/createCategories";
+import { listProducts } from "./app/useCases/products/listProducts";
+import { createProducts } from "./app/useCases/products/createProducts";
+
+export const routes = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename: function (req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    },
+  }),
+})
+
+// List categories
+routes.get("/categories", listCategories);
+
+// Create category
+routes.post("/categories", createCategories);
+
+// List Products
+routes.get("/products", listProducts);
+
+// Create Product
+routes.post("/products", upload.single('image'), createProducts);
+
+// Get products by category
+routes.get("/categories/:categoryId/products", (req, res) => {
+  res.send('OK');
+});
+
+// List orders
+routes.get("/orders", (req, res) => {
+  res.send('OK');
+});
+
+// Create order
+routes.post("/orders", (req, res) => {
+  res.send('OK');
+});
+
+// Change order status
+routes.patch("/orders/:orderId", (req, res) => {
+  res.send('OK');
+});
+
+// Delete order
+routes.delete("/orders/:orderId", (req, res) => {
+  res.send('OK');
+});
